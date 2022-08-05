1c95455846beb8f08f9da79262bcc2e96c3f81d3
Angela Zoss <angela.zoss@gmail.com>
2022-08-05 13:12:46 -0400

    add factoring to data load script

--- data/inclusiveness_index/load_inclusiveness_index.R
+++ data/inclusiveness_index/load_inclusiveness_index.R
@@ -5,4 +5,6 @@ inclusiveness_index <- read_excel('../data/inclusiveness_index/global_data_for_w
                                   na="9999") %>%
   rename_with(~ str_remove_all(.x, "[(|)]") %>% str_replace_all("[-| ]", ".")) %>%
   dplyr::filter(Continent != "Antarctica") %>% 
-  mutate(Continent = Continent %>% as_factor() %>% fct_infreq())
+  mutate(Continent = Continent %>% as_factor() %>% fct_infreq(),
+         Index.categories.2020 = Index.categories.2020 %>% as_factor() %>% 
+           fct_relevel(c("High", "Medium-High", "Medium", "Medium-Low", "Low", "No data")))
